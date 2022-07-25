7ab354fe8b4c48a95611d7f07b938c85278ad878
Eugine <36677719+Astroner@users.noreply.github.com>
2022-07-25 04:18:49 +0300

    smol fix in ast comment

--- ast/ast.c
+++ ast/ast.c
@@ -77,7 +77,7 @@ static ASTNode* createUnaryExpression(
 static ASTNode* parseTokenExpression(
     List* tokens,           // Token List
     BOOL breakOnBracket,    // TRUE if u want to stop parsing on first closing bracket
-    BOOL withLength,        // Should function store length of parsed tokens into lengthPointer
+    BOOL withLength,        // Should function store count of parsed tokens into lengthPointer
     int* lengthPointer,     // Pointer for length. Primary used for skip variable
     ASTNode* __first,       // Internal recursion arg for operation prior handling when we already have first member.
     int limit               // Limit for list reading; -1 means no limit
